6517da22c561a917a5c3bd4d6925095f63f4809f
Manuel Strehl <git@manuel-strehl.de>
2016-05-04 00:46:24 +0200

    spice up the renderer a bit

--- src/components/renderer.js
+++ src/components/renderer.js
@@ -4,12 +4,26 @@
 import message_store from './message_store';
 
 
+const warnings_cache = new WeakMap();
+
+
 const Renderer = {
 
   show_warning: function(element) {
     const msg = message_store.get(element);
+    var warning = warnings_cache.get(element);
     if (msg) {
-      window.alert(msg);
+      if (! warning) {
+        warning = document.createElement('div');
+        warning.className = 'hf-warning';
+        warnings_cache.set(element, warning);
+      }
+      warning.textContent = msg;
+      /* should also work, if element is last,
+       * http://stackoverflow.com/a/4793630/113195 */
+      element.parentNode.insertBefore(warning, element.nextSibling);
+    } else if (warning && warning.parentNode) {
+      warning.parentNode.removeChild(warning);
     }
   },
 
