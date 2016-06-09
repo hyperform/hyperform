74e01192341b94eaab1c1322ddf8e69ad496f081
Manuel Strehl <git@manuel-strehl.de>
2016-06-09 13:17:40 +0200

    more tests for validityState

--- test/polyfills/validityState.js
+++ test/polyfills/validityState.js
@@ -15,3 +15,41 @@ test('ValidityState customError non-bool response', t => {
   Registry.set(input, () => 0);
   t.true(ValidityState(input).customError);
 });
+
+test('ValidityState rangeOverflow', t => {
+  var input = document.createElement('input');
+
+  input.value = 5;
+  t.false(ValidityState(input).rangeOverflow);
+
+  input.type = 'number'
+  input.max = 2;
+  t.true(ValidityState(input).rangeOverflow);
+
+  input.value = 2;
+  t.false(ValidityState(input).rangeOverflow);
+
+  input.max = 1;
+  t.true(ValidityState(input).rangeOverflow);
+
+  input.value = '';
+  t.false(ValidityState(input).rangeOverflow);
+});
+
+test('ValidityState patternMismatch', t => {
+  var input = document.createElement('input');
+  t.false(ValidityState(input).patternMismatch);
+
+  input.pattern = 'X';
+  input.value = 'Y';
+  t.true(ValidityState(input).patternMismatch);
+
+  input.value = '';
+  t.false(ValidityState(input).patternMismatch);
+
+  input.value = 'X';
+  t.false(ValidityState(input).patternMismatch);
+
+  input.value = 'XA';
+  t.true(ValidityState(input).patternMismatch);
+});
