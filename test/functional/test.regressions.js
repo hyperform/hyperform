4a1f523d9dd2bd537c32772e09241f18183e0cd6
Manuel Strehl <git@manuel-strehl.de>
2017-01-23 22:04:51 +0100

    fix first functional tests

--- test/functional/test.regressions.js
+++ test/functional/test.regressions.js
@@ -1,5 +1,6 @@
 var form, hform;
 
+
 beforeEach(function() {
   form = document.createElement('form');
   form.innerHTML = '<input name="test" value="button_span">'+
@@ -7,6 +8,7 @@ beforeEach(function() {
   hform = hyperform(form);
   document.body.appendChild(form);
 });
+
 
 afterEach(function() {
   if (form) {
@@ -17,12 +19,51 @@ afterEach(function() {
   }
 });
 
+
+describe('Issue 13', function() {
+
+  it('should add name=value of submit button', function(done) {
+    form.method = 'get';
+    form.addEventListener('submit', function(evt) {
+      if (! evt.submittedVia || evt.submittedVia.nodeName !== 'BUTTON') {
+        throw Error('not a Hyperform submit event');
+      }
+    });
+
+    const button = form.querySelector('button');
+    button.name = 'test';
+    button.value = 'value';
+
+    const iframe = document.createElement('iframe');
+    iframe.src = './blank.html';
+    document.body.append(iframe);
+    form.parentNode.removeChild(form);
+    iframe.contentDocument.body.appendChild(form);
+    form.action = 'blank.html';
+
+    function run() {
+      iframe.removeEventListener('load', run);
+
+      if (iframe.contentWindow.location.search.search(/test=value/) === -1) {
+        throw Error('test value not found');
+      }
+      iframe.parentNode.removeChild(iframe);
+      done();
+    }
+
+    iframe.addEventListener('load', run);
+    button.click();
+  });
+
+});
+
+
 describe('Issue 34', function() {
 
   it('should submit when clicking on span nested in button', function(done) {
     form.addEventListener('submit', function(evt) {
       evt.preventDefault();
-      if (! evt.submittedVia || evt.submittedVia.nodeName !== 'button') {
+      if (! evt.submittedVia || evt.submittedVia.nodeName !== 'BUTTON') {
         throw Error('not a Hyperform submit event');
       }
       done();
