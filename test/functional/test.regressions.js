e8ab2aedecd807471c6f5263a010bd4dc1e5513b
Manuel Strehl <git@manuel-strehl.de>
2017-02-01 11:22:45 +0100

    fix tests for IE 11

--- test/functional/test.regressions.js
+++ test/functional/test.regressions.js
@@ -1,60 +1,57 @@
-var form, hform;
-
-
-beforeEach(function() {
+function make_hform(_doc) {
+  _doc = _doc || document;
   form = document.createElement('form');
   form.innerHTML = '<input name="test" value="button_span">'+
                    '<button><span>submit</span></button>';
-  hform = hyperform(form);
-  document.body.appendChild(form);
-});
+  hform = _doc.defaultView.hyperform(form);
+  _doc.body.appendChild(form);
+  return hform;
+}
 
 
-afterEach(function() {
-  if (form) {
-    if (form.parentNode) {
-      form.parentNode.removeChild(form);
-    }
-    hform.destroy();
-    form = undefined;
-    hform = undefined;
-  }
-});
+function once(element, event, handler) {
+  var func = function(evt) {
+    handler(evt);
+    element.removeEventListener(event, func);
+  };
+  element.addEventListener(event, func);
+}
 
 
 describe('Issue 13', function() {
 
   it('should add name=value of submit button', function(done) {
-    form.method = 'get';
-    form.addEventListener('submit', function(evt) {
-      if (! evt.submittedVia || evt.submittedVia.nodeName !== 'BUTTON') {
-        throw Error('not a Hyperform submit event');
-      }
-    });
-
-    const button = form.querySelector('button');
-    button.name = 'test';
-    button.value = 'value';
-
     const iframe = document.createElement('iframe');
     iframe.src = './blank.html';
     document.body.appendChild(iframe);
-    form.parentNode.removeChild(form);
-    iframe.contentDocument.body.appendChild(form);
-    form.action = 'blank.html';
 
-    function run() {
-      iframe.removeEventListener('load', run);
+    once(iframe, 'load', function() {
+      const form = make_hform(iframe.contentDocument).form;
+      form.method = 'get';
+      form.addEventListener('submit', function(evt) {
+        if (! evt.submittedVia || evt.submittedVia.nodeName !== 'BUTTON') {
+          throw Error('not a Hyperform submit event');
+        }
+      });
 
-      if (iframe.contentWindow.location.search.search(/test=value/) === -1) {
-        throw Error('test value not found');
-      }
-      iframe.parentNode.removeChild(iframe);
-      done();
-    }
+      const button = form.querySelector('button');
+      button.name = 'test';
+      button.value = 'value';
 
-    iframe.addEventListener('load', run);
-    button.click();
+      form.parentNode.removeChild(form);
+      iframe.contentDocument.body.appendChild(form);
+      form.action = 'blank.html';
+
+      once(iframe, 'load', function() {
+        if (iframe.contentWindow.location.search.search(/test=value/) === -1) {
+          throw Error('test value not found');
+        }
+        iframe.parentNode.removeChild(iframe);
+        done();
+      });
+
+      button.click();
+    });
   });
 
 });
@@ -63,6 +60,7 @@ describe('Issue 13', function() {
 describe('Issue 34', function() {
 
   it('should catch submit when clicking on span nested in button', function(done) {
+    const form = make_hform().form;
     form.addEventListener('submit', function(evt) {
       evt.preventDefault();
       if (! evt.submittedVia || evt.submittedVia.nodeName !== 'BUTTON') {
