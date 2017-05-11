28814c1b46cacc3a22071c24bfc37e404239d841
Manuel Strehl <git@manuel-strehl.de>
2017-05-11 13:44:04 +0200

    add test for base lang localization

--- test/unit/components/localization.js
+++ test/unit/components/localization.js
@@ -7,15 +7,26 @@ test('localization', t => {
   set_language('fi');
 
   let canary = 'Never translate this string! #+*~';
+
+  // return string unchanged
   t.is(_(canary), canary);
 
   add_translation('fi', { '#__testing__': canary+canary, });
+
+  // return base string still unchanged, but
   t.is(_(canary), canary);
+  // translate the testing string
+  t.is(_('#__testing__'), canary+canary);
+
+  set_language('fi-ZH');
+  // return the base lang translation, if it exists
   t.is(_('#__testing__'), canary+canary);
 
   set_language('bar');
+  // return the testing string after switching language
   t.is(_('#__testing__'), '#__testing__');
 
-  add_translation('en', { '#__testing__': canary+canary, });
-  t.is(_('#__testing__'), canary+canary);
+  add_translation('en', { '#__testing__': canary+canary+canary, });
+  // return the english default, when it exists
+  t.is(_('#__testing__'), canary+canary+canary);
 });
