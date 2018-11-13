a1bd47938dac6acf3aad673b3bc7aa33bd58b61c
Casey Corcoran <github@snaptopixel.com>
2018-11-13 11:25:44 -0500

    comments

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,22 @@
 interface HyperFormOptions {
   /** When true, disable the high-level API. Default: false. */
   strict?: boolean;
+
   /** Prevent Enter in input fields to submit the form. Default: false. */
   preventImplicitSubmit?: boolean;
+
   /** Whether and when fields should be re-validated automatically. */
-  revalidate?: 'oninput' | 'onblur' | 'hybrid' | 'onsubmit' | 'never'
+  revalidate?: 'oninput' | 'onblur' | 'hybrid' | 'onsubmit' | 'never';
+
   /** Whether the non-standard valid event should be triggered. Default: true. */
   validEvent?: boolean;
+
   /** Whether the <fieldset> element should be treated like a normal input field, e.g. allowing it to receive an error message. Default: true. */
   extendFieldset?: boolean;
+
   /** Whether input fields with the non-standard attribute novalidate should be exempted from validation. Default: true. */
   novalidateOnElements?: boolean;
+
   /** CSS class names to use instead of the default ones. */
   classes?: {
     warning?: string,
@@ -18,14 +24,22 @@ interface HyperFormOptions {
     invalid?: string,
     validated?: string
   }
+  
   /** Whether to include input elements without name attribute as validation candidates. Default: false. */
   validateNameless?: boolean;
 }
 
 export interface HyperFormRenderer {
+  /** called when a warning should become visible */
   attachWarning: (warning: HTMLElement, element: HTMLElement) => void;
+
+  /** called when a warning should vanish */
   detachWarning: (warning: HTMLElement, element: HTMLElement) => void;
+
+  /** called when feedback to an element's state should be handled ie: showing and hiding warnings */
   showWarning: (element: HTMLElement, wholeFormValidated: boolean) => void;
+
+  /** set the warning's content */
   setMessage: (warning: HTMLElement, message: string, element: HTMLElement) => void;
 }
 
