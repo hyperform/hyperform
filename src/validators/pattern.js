a6a36f1bb4e7139b9bf0ebcdf616f1dd9bbb5576
Manuel Strehl <git@manuel-strehl.de>
2023-05-16 10:03:24 +0200

    fix patterns having the "u" flag applied

--- src/validators/pattern.js
+++ src/validators/pattern.js
@@ -1,8 +1,21 @@
 'use strict';
 
+let canUnicode = true;
+try {
+  /* for old IEs downgrade to non-Unicode supporting regexps. This trades a
+   * correct solution importing huge solutions like regexpu for wrong patterns
+   * in a small number of cases for old, outdated browsers. */
+  new RegExp('a', 'u');
+} catch (e) {
+  canUnicode = false;
+}
+
 
 /**
  * test the pattern attribute
+ *
+ * According to the spec, the "u" flag has to be set and regular expressions
+ * are anchored (i.e. prepended with `^` and appended an `$`).
  */
 export default function(element) {
   return (
@@ -10,6 +23,6 @@ export default function(element) {
       ||
       ! element.hasAttribute('pattern')
       ||
-      (new RegExp('^(?:'+ element.getAttribute('pattern') +')$')).test(element.value)
+      (new RegExp('^(?:'+ element.getAttribute('pattern') +')$'), canUnicode? 'u' : undefined).test(element.value)
     );
 }
